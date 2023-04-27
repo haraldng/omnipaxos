97a43aec5cb704f7639c953f9b6fe5ce3b46824a
Harald Ng <harald.ng@hotmail.com>
2023-04-27 20:17:53 +0200

    Add test for macro

---
+++ omnipaxos_macros/tests/derive_test.rs
@@ -0,0 +1,24 @@
+#[macro_use]
+extern crate omnipaxos_macros;
+
+#[test]
+fn derive_entry_test() {
+    use omnipaxos_core::omni_paxos::{OmniPaxos, OmniPaxosConfig};
+    use omnipaxos_storage::memory_storage::MemoryStorage;
+
+    #[derive(Clone, Debug, Entry)]
+    struct TestEntry {
+        pub _field1: u64,
+        pub _field2: String,
+    }
+
+    let config = OmniPaxosConfig {
+        configuration_id: 1,
+        pid: 1,
+        peers: vec![2, 3],
+        ..Default::default()
+    };
+
+    let _omnipaxos: OmniPaxos<TestEntry, MemoryStorage<TestEntry>> =
+        config.build(MemoryStorage::default());
+}
