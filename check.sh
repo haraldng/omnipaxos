94cde8da16fb3cf8e64ff89fc62556f9723c4edb
Kevin <kevin@harrisoncentral.com>
2023-04-27 08:17:48 +0200

    Simplified checks and improved docs

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
-cargo test --features "logging, toml_config" --workspace --all-targets
-cargo +nightly fmt --all -- --check
+cargo test --workspace --all-targets
+cargo fmt --all -- --check
 cargo check --workspace --all-targets
 #cargo clippy --workspace --all-targets -- -D warnings -W clippy::all
 cargo clippy -- -D warnings -W clippy::all
