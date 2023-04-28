d8c9b43150d15e0b04de3db07577f0b8aec87cdb
Harald Ng <harald.ng@hotmail.com>
2023-04-28 11:29:59 +0200

    Update docs for features

--- omnipaxos_storage/src/lib.rs
+++ omnipaxos_storage/src/lib.rs
@@ -1,7 +1,9 @@
 //! A library of storage implementations for SequencePaxos
 
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![deny(missing_docs)]
 /// an in-memory storage implementation with fast read and writes
 pub mod memory_storage;
+
 /// an on-disk storage implementation with persistence for the replica state and the log.
 pub mod persistent_storage;
