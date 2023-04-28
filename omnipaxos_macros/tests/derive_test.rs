288cd4750d688edcc6ff8d155222fa5601e92fed
Harald Ng <harald.ng@hotmail.com>
2023-04-28 09:52:47 +0200

    Update derive_test.rs

--- omnipaxos_macros/tests/derive_test.rs
+++ omnipaxos_macros/tests/derive_test.rs
@@ -3,6 +3,15 @@ extern crate omnipaxos_macros;
 
 #[test]
 fn derive_entry_test() {
+    #[derive(Clone, Debug, Entry)]
+    struct TestEntry {
+        pub _field1: u64,
+        pub _field2: String,
+    }
+}
+
+#[test]
+fn build_op_test() {
     use omnipaxos_core::omni_paxos::{OmniPaxos, OmniPaxosConfig};
     use omnipaxos_storage::memory_storage::MemoryStorage;
 
