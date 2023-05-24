db6fbbfc553ffa01e28eb67cd4f5c8282f226924
Tim-XL-Han <tim.han.0000@gmail.com>
2023-05-24 10:43:49 +0200

    Turned back to js

--- check.sh
+++ check.sh
@@ -7,7 +7,3 @@ cargo check --workspace --all-targets
 #cargo clippy --workspace --all-targets -- -D warnings -W clippy::all
 cargo clippy -- -D warnings -W clippy::all
 cargo test --features "logging, toml_config" --workspace --doc
-
-# Check doc structure defination
-chmod +x scripts/doc_structure_checker
-scripts/doc_structure_checker
