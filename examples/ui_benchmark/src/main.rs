4f61490308a59f1870496177478c077e4c249c27
TimXLHan <tim.han.0000@gmail.com>
2023-08-14 10:43:12 +0200

    Added ui benchmark as bin

--- examples/ui_benchmark/src/main.rs
+++ examples/ui_benchmark/src/main.rs
@@ -1,23 +1,25 @@
 // To run the benchmark for ui:
-// cargo test --test ui_test  -- --nocapture
+// cargo run --bin ui_benchmark
 
-use omnipaxos_macros::Entry;
+use omnipaxos::macros::Entry;
 use omnipaxos::{ClusterConfig, OmniPaxos, OmniPaxosConfig, ServerConfig};
 use omnipaxos_storage::memory_storage::MemoryStorage;
 use std::thread;
 use std::time::Duration;
 
-pub mod utils;
 
 #[derive(Entry, Clone, Debug)]
 struct Entry {}
 
-fn op_buildup() -> OmniPaxos<Entry, MemoryStorage<Entry>> {
-    const SERVERS: [u64; 3] = [1, 2, 3];
-    const ELECTION_TICK_TIMEOUT: u64 = 10;
-    const UI_UPDATE_TICK_TIMEOUT: u64 = 50;
-    const RESEND_MESSAGE_TICK_TIMEOUT: u64 = 100;
+// Tick rate of `TICK_RATE ms/tick`
+const TICK_RATE: u64 = 1;
+const SERVERS: [u64; 3] = [1, 2, 3];
+const ELECTION_TICK_TIMEOUT: u64 = 10;
+const UI_UPDATE_TICK_TIMEOUT: u64 = 50;
+const RESEND_MESSAGE_TICK_TIMEOUT: u64 = 100;
+const NUMBER_OF_TICKS: u64 = 1000;
 
+fn op_buildup() -> OmniPaxos<Entry, MemoryStorage<Entry>> {
     let server_config = ServerConfig {
         pid: SERVERS[0],
         election_tick_timeout: ELECTION_TICK_TIMEOUT,
@@ -38,44 +40,47 @@ fn op_buildup() -> OmniPaxos<Entry, MemoryStorage<Entry>> {
     omni_paxos
 }
 
-#[test]
 fn bench_tick_with_ui() {
     let mut op = op_buildup();
     op.start_ui();
     // Tick with UI started
     let now = std::time::Instant::now();
-    for _ in 0..100 {
+    for _ in 0..NUMBER_OF_TICKS {
         op.tick();
     }
     let tick_ui_timeout = now.elapsed().as_nanos();
 
     // Tick with UI started and 1ms/tick
     let now = std::time::Instant::now();
-    for _ in 0..100 {
+    for _ in 0..NUMBER_OF_TICKS {
         op.tick();
-        thread::sleep(Duration::from_millis(1));
+        thread::sleep(Duration::from_millis(TICK_RATE));
     }
     let tick_1ms_ui_timeout = now.elapsed().as_millis();
     op.stop_ui();
 
     // Tick with UI stopped
     let now = std::time::Instant::now();
-    for _ in 0..100 {
+    for _ in 0..NUMBER_OF_TICKS {
         op.tick();
     }
     let tick_timeout = now.elapsed().as_nanos();
 
     // Tick with UI stopped and 1ms/tick
     let now = std::time::Instant::now();
-    for _ in 0..100 {
+    for _ in 0..NUMBER_OF_TICKS {
         op.tick();
-        thread::sleep(Duration::from_millis(1));
+        thread::sleep(Duration::from_millis(TICK_RATE));
     }
     let tick_1ms_timeout = now.elapsed().as_millis();
 
     // Output results
-    println!("Time elapsed of 100 ticks with UI started: {:.2?} nanoseconds", tick_ui_timeout);
-    println!("Time elapsed of 100 ticks without UI: {:.2?} nanoseconds", tick_timeout);
-    println!("Time elapsed of 100 ticks with UI started and 1ms/tick: {:.2?} ms", tick_1ms_ui_timeout);
-    println!("Time elapsed of 100 ticks without UI and 1ms/tick: {:.2?} ms", tick_1ms_timeout);
+    println!("Time elapsed of {:?} ticks with UI started: {:.2?} nanoseconds", NUMBER_OF_TICKS, tick_ui_timeout);
+    println!("Time elapsed of {:?} ticks without UI: {:.2?} nanoseconds", NUMBER_OF_TICKS, tick_timeout);
+    println!("Time elapsed of {:?} ticks with UI started and {:?}ms/tick: {:.2?} ms", NUMBER_OF_TICKS, TICK_RATE, tick_1ms_ui_timeout);
+    println!("Time elapsed of {:?} ticks without UI and {:?}ms/tick: {:.2?} ms", NUMBER_OF_TICKS, TICK_RATE, tick_1ms_timeout);
+}
+
+fn main() {
+    bench_tick_with_ui();
 }
