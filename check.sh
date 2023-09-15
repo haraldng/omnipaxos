ac2e888d85b257c5c96f17df29c468848d54e6eb
Harald Ng <harald.ng@hotmail.com>
2023-09-15 18:08:46 +0200

    Add checks for all possible features and clippy for all workspaces

--- check.sh
+++ check.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eux
 
+cd omnipaxos ; ./test_all_features.sh -c ; cd ..
 cargo test --features "logging, toml_config, macros" --workspace --all-targets
 cargo fmt --all -- --check
 cargo check --workspace --all-targets
-#cargo clippy --workspace --all-targets -- -D warnings -W clippy::all
-cargo clippy -- -D warnings -W clippy::all
+cargo clippy --workspace --all-targets -- -D warnings -W clippy::all
 cargo test --features "logging, toml_config" --workspace --doc
