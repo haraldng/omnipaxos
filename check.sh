d89264f81ffd470b127bcdb0fa500fb856055793
Harald Ng <harald.ng@hotmail.com>
2023-11-07 15:10:19 +0100

    Add check for crates badge version

--- check.sh
+++ check.sh
@@ -8,3 +8,29 @@ cargo test --features "logging, toml_config, unicache" -p omnipaxos --all-target
 cargo fmt --all -- --check
 cargo clippy --workspace --all-targets -- -D warnings -W clippy::all
 cargo test --features "logging, toml_config" --workspace --doc
+
+# --------------------------------------------------------------
+# Check cargo badge version corresponds to the one defined in Cargo.toml
+# --------------------------------------------------------------
+
+set +x
+CARGO_TOML_PATH="omnipaxos/Cargo.toml"
+# Extract the version from Cargo.toml
+CURRENT_VERSION=$(grep -m 1 "version" "$CARGO_TOML_PATH" | sed -E 's/.*"([^"]+)"/\1/')
+# Extract the version from the README badge
+README_BADGE_VERSION=$(perl -n -e '/crates.io-v([\d.]+)/ && print $1' README.md)
+
+if [ -z "$CURRENT_VERSION" ]; then
+  echo "Error: Unable to extract version from $CARGO_TOML_PATH"
+  exit 1
+fi
+
+if [ -z "$README_BADGE_VERSION" ]; then
+  echo "Error: Unable to extract version from README badge"
+  exit 1
+fi
+
+if [ "$CURRENT_VERSION" != "$README_BADGE_VERSION" ]; then
+  echo "Error: Version in Cargo.toml ($CURRENT_VERSION) does not match the version in README badge ($README_BADGE_VERSION)."
+  exit 1
+fi
--- crates-checklist.md
+++ crates-checklist.md
@@ -1,9 +1,10 @@
 # Crates Release Checklist
 1. Update the version number for the crates badge in `README.md`
-2. Update version number in `Cargo.toml` as required for each of the crates: `omnipaxos`, `omnipaxos_macros`, `omnipaxos_storage`, and `omnipaxos_ui` 
-3. Make sure that `./check.sh` passes
-4. Run `cargo publish --dry-run` on each of the crates.
-5. The crates have inter-dependencies, so to publish them, we run `cargo publish` in the following order: 
+2. Update version number in `Cargo.toml` as required for each of the crates: `omnipaxos`, `omnipaxos_macros`, `omnipaxos_storage`, and `omnipaxos_ui`.
+3. Update the version number for the crate badge in the README.
+4. Make sure that `./check.sh` passes
+5. Run `cargo publish --dry-run` on each of the crates.
+6. The crates have inter-dependencies, so to publish them, we run `cargo publish` in the following order: 
    1. `omnipaxos_macros`
    2. `omnipaxos`
    3. `omnipaxos_storage`
