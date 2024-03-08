961ba9f3bce1ae168c5043c25cb91de14c5d37bb
Harald Ng <harald.ng@hotmail.com>
2024-03-08 16:08:20 +0100

    Add temporary UniCacheSerdeEntry macro

--- omnipaxos_macros/src/lib.rs
+++ omnipaxos_macros/src/lib.rs
@@ -165,7 +165,6 @@ pub fn unicache_entry_derive(input: TokenStream) -> TokenStream {
                 }
 
                 #[derive(Clone, Debug)]
-                #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
                 pub struct #impl_generics #cache_name #ty_generics #where_clause {
                     #(#cache_fields,)*
                 }
@@ -200,6 +199,149 @@ pub fn unicache_entry_derive(input: TokenStream) -> TokenStream {
         syn::Data::Union(..) => unreachable!(),
     }
     .into()
+}
+
+/// Derive macro for UniCache when used with omnipaxos and serde.
+/// This is just a temp-fix for the `serde` feature. It should be removed once we find a way to check if omnipaxos is compiled with the `serde` feature.
+#[proc_macro_derive(UniCacheSerdeEntry, attributes(unicache, snapshot))]
+pub fn unicache_serde_entry_derive(input: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let snapshot_type = get_snapshot_type(&ast);
+    let mut cache_type = None;
+
+    match ast.data {
+        syn::Data::Struct(data) => {
+            let mut field_names = Vec::new();
+
+            let mut encodable_field_names = Vec::new();
+            let mut encodable_field_types = Vec::new();
+            let mut encodable_field_attr_types = Vec::new();
+            let mut encode_result = Vec::new();
+
+            let mut non_encodable_field_names = Vec::new();
+            let mut non_encodable_field_types = Vec::new();
+
+            let mut cache_fields = Vec::new();
+            let mut cache_sizes = Vec::new();
+            let mut cache_types = Vec::new();
+
+            for field in &data.fields {
+                let name = field.ident.as_ref().unwrap();
+                let ty = &field.ty;
+                let attr = field.attrs.iter().find(|attr| {
+                    attr.path().is_ident("unicache")
+                });
+                field_names.push(name);
+                if let Some(attr) = attr {
+                    let mut encoding_type = None;
+                    let mut cache_size = None;
+                    attr.parse_nested_meta(|meta| {
+                        let path = meta.path;
+                        if path.is_ident("size") {
+                            let content;
+                            syn::parenthesized!(content in meta.input);
+                            let lit: syn::LitInt = content.parse()?;
+                            let n: usize = lit.base10_parse()?;
+                            cache_size = Some(quote::quote!(#n));
+                        } else if path.is_ident("cache") {
+                            let content;
+                            syn::parenthesized!(content in meta.input);
+                            let ty: syn::Ident = content.parse()?;
+                            // cache_type = Some(quote::quote!(#ty));
+                            // let id = attr
+                            //     .parse_args::<syn::Ident>()
+                            //     .expect("Expected identifier");
+                            cache_type = if ty == "lru" {
+                                Some(quote!(::omnipaxos::unicache::lru_cache::LRUniCache))
+                            } else if ty == "lfu" {
+                                Some(quote!(::omnipaxos::unicache::lfu_cache::LFUniCache))
+                            } else {
+                                panic!("Invalid cache type")
+                            };
+                        } else if path.is_ident("encoding") {
+                            let content;
+                            syn::parenthesized!(content in meta.input);
+                            let ty: syn::Ident = content.parse()?;
+                            encoding_type = Some(quote::quote!(#ty));
+                        } else {
+                            panic!("Found unexpected attribute `{}`", quote::quote!(#path))
+                        }
+                        Ok(())
+                    }).unwrap_or_else(|_| panic!("Expected a valid attribute {}", quote::quote!(#attr)));
+                    // Defaults
+                    encoding_type = encoding_type.take().or_else(|| Some(quote!(u8)));
+                    cache_size = cache_size.take().or_else(|| Some(quote::quote!(u8::MAX as usize)));
+                    cache_type = cache_type.take().or_else(|| Some(quote::quote!(::omnipaxos::unicache::lru_cache::LRUniCache)));
+
+                    encodable_field_names.push(name);
+                    encodable_field_attr_types.push(encoding_type.clone());
+                    encodable_field_types.push(ty);
+                    encode_result.push(quote!(MaybeEncoded<#ty, #encoding_type>));
+                    cache_fields.push(quote!(#name: #cache_type<#ty, #encoding_type>));
+                    cache_sizes.push(cache_size);
+                    cache_types.push(cache_type.clone());
+                } else {
+                    non_encodable_field_names.push(name);
+                    non_encodable_field_types.push(ty);
+                    encode_result.push(quote!(#ty));
+                }
+            }
+
+            let cache_name = suffix(name, "Cache");
+
+            quote! {
+                use ::omnipaxos::unicache::FieldCache;      // TODO remove this and use fully qualified path in the method calls instead?
+
+                impl #impl_generics Entry for #name #ty_generics #where_clause {
+                    type Snapshot = #snapshot_type;
+                    type Encoded = (#(#encodable_field_attr_types,)*);
+                    type Encodable = (#(#encodable_field_types,)*);
+                    type NotEncodable = (#(#non_encodable_field_types,)*);
+                    type EncodeResult = (#(#encode_result,)*);
+                    type UniCache = #cache_name #ty_generics;
+                }
+
+                #[derive(Clone, Debug, Serialize, Deserialize)]
+                // #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+                pub struct #impl_generics #cache_name #ty_generics #where_clause {
+                    #(#cache_fields,)*
+                }
+
+                impl #impl_generics UniCache for #cache_name #ty_generics #where_clause {
+                    type T = #name #ty_generics;
+
+                    fn new() -> Self {
+                        Self {
+                            #(#encodable_field_names: #cache_types::new(#cache_sizes),)*
+                        }
+                    }
+
+                    fn try_encode(&mut self, entry: &Self::T) -> <Self::T as Entry>::EncodeResult {
+                        #(let #encodable_field_names = self.#encodable_field_names.try_encode(&entry.#encodable_field_names);)*
+                        #(let #non_encodable_field_names = entry.#non_encodable_field_names.clone();)*
+                        (#(#field_names,)*)
+                    }
+
+                    fn decode(&mut self, (#(#field_names,)*): <Self::T as Entry>::EncodeResult) -> Self::T {
+                        Self::T {
+                            #(#encodable_field_names: self.#encodable_field_names.decode(#encodable_field_names),)*
+                            #(#non_encodable_field_names,)*
+                        }
+                    }
+                }
+            }
+        }
+        syn::Data::Enum(_data) => {
+            todo!()
+        }
+        syn::Data::Union(..) => unreachable!(),
+    }
+        .into()
 }
 
 fn suffix(ident: &Ident, name: &str) -> syn::Ident {
