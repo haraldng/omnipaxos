38b768965153975d45e32aafd83616c5b5918bf2
Harald Ng <harald.ng@hotmail.com>
2024-10-10 15:33:29 -0700

    Add comments for Metronome code

--- omnipaxos/src/storage/metronome.rs
+++ omnipaxos/src/storage/metronome.rs
@@ -1,5 +1,4 @@
-use crate::storage::Entry;
-use crate::util::NodeId;
+use crate::{storage::Entry, util::NodeId};
 use itertools::Itertools;
 use std::collections::HashSet;
 
@@ -15,29 +14,36 @@ pub(crate) struct Metronome {
 
 impl Metronome {
     pub(crate) fn get_critical_ordering(&self) -> Vec<usize> {
-        self.my_ordering.iter().take(self.critical_len).cloned().collect()
+        self.my_ordering
+            .iter()
+            .take(self.critical_len)
+            .cloned()
+            .collect()
     }
 }
 
 type QuorumTuple = Vec<NodeId>;
 
 impl Metronome {
+    /// Takes all possible quorum combinations and returns an ordering of quorums that maximizes the distance between consecutive quorums
     fn maximize_distance_ordering(tuples: &mut Vec<QuorumTuple>) -> Vec<QuorumTuple> {
         let mut ordered_tuples = vec![tuples.remove(0)];
 
         while !tuples.is_empty() {
-            let valid_tuples: Vec<_> = tuples
+            // try order quorums such that there is no common node with the last quorum
+            let no_repeat_tuples: Vec<_> = tuples
                 .iter()
                 .filter(|t| {
                     let t_set: HashSet<_> = t.iter().collect();
                     let last_set: HashSet<_> = ordered_tuples.last().unwrap().iter().collect();
-                    t_set.intersection(&last_set).count() == 0
+                    t_set.intersection(&last_set).count() == 0 // no common nodes
                 })
                 .cloned()
                 .collect();
 
-            let next_tuple = if !valid_tuples.is_empty() {
-                valid_tuples
+            let next_tuple = if !no_repeat_tuples.is_empty() {
+                // there is a quorum that has no common nodes with the last quorum, pick the one with the max distance (probably not necessary)
+                no_repeat_tuples
                     .into_iter()
                     .max_by(|t1, t2| {
                         let dist1 = Self::distance(ordered_tuples.last().unwrap(), t1);
@@ -46,6 +52,7 @@ impl Metronome {
                     })
                     .unwrap()
             } else {
+                // all quorums have common nodes with the last quorum, pick the one with the max distance
                 tuples
                     .iter()
                     .max_by(|t1, t2| {
@@ -66,7 +73,11 @@ impl Metronome {
     }
 
     fn distance(t1: &QuorumTuple, t2: &QuorumTuple) -> usize {
-        assert_eq!(t1.len(), t2.len(), "Vectors must have the same dimension for distance calculation");
+        assert_eq!(
+            t1.len(),
+            t2.len(),
+            "Vectors must have the same dimension for distance calculation"
+        );
 
         let sum_of_squares: usize = t1
             .iter()
@@ -79,21 +90,29 @@ impl Metronome {
     }
 
     fn create_ordered_quorums(num_nodes: usize, quorum_size: usize) -> Vec<QuorumTuple> {
-        let mut quorum_combos = (1..=num_nodes as NodeId).combinations(quorum_size).collect();
+        let mut quorum_combos = (1..=num_nodes as NodeId)
+            .combinations(quorum_size)
+            .collect();
         Self::maximize_distance_ordering(&mut quorum_combos)
     }
 
-    fn get_my_ordering_and_critical_len(my_pid: NodeId, ordered_quorums: &Vec<QuorumTuple>) -> (Vec<usize>, usize) {
+    /// Takes the ordered quorums and returns the ordering of the quorums for this node and the critical length
+    fn get_my_ordering_and_critical_len(
+        my_pid: NodeId,
+        ordered_quorums: &Vec<QuorumTuple>,
+    ) -> (Vec<usize>, usize) {
         let mut ordering = Vec::with_capacity(ordered_quorums.len());
-        let mut rest = Vec::with_capacity(ordered_quorums.len()/2);
+        let mut rest = Vec::with_capacity(ordered_quorums.len() / 2);
         let mut critical_len = 0;
-        for (qid, q) in ordered_quorums.iter().enumerate() {
+        for (entry_id, q) in ordered_quorums.iter().enumerate() {
             if q.contains(&my_pid) {
-                ordering.push(qid);
+                ordering.push(entry_id);
             } else {
-                rest.push(qid);
+                // I'm not in this quorum i.e., this entry is not part of my "critical batch"
+                rest.push(entry_id);
             }
-            if critical_len == 0 && qid == ordered_quorums.len()-1 {
+            // set the critical length when processing the last entry
+            if critical_len == 0 && entry_id == ordered_quorums.len() - 1 {
                 critical_len = ordering.len();
             }
         }
@@ -105,27 +124,29 @@ impl Metronome {
         todo!()
     }
 
-
     pub(crate) fn with(pid: NodeId, num_nodes: usize, quorum_size: usize) -> Self {
         let ordered_quorums = Self::create_ordered_quorums(num_nodes, quorum_size);
-        let (ordering, critical_len) = Self::get_my_ordering_and_critical_len(pid, &ordered_quorums);
+        let (ordering, critical_len) =
+            Self::get_my_ordering_and_critical_len(pid, &ordered_quorums);
         Metronome {
             pid,
             my_ordering: ordering,
             all_orderings: vec![],
-            critical_len
+            critical_len,
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
     use super::*;
+    use std::collections::HashMap;
 
     fn check_critical_len(all_metronomes: &Vec<Metronome>) {
         let critical_len = all_metronomes[0].critical_len;
-        let all_same_critical_len = all_metronomes.iter().all(|m| m.critical_len == critical_len);
+        let all_same_critical_len = all_metronomes
+            .iter()
+            .all(|m| m.critical_len == critical_len);
         assert!(all_same_critical_len);
         let num_nodes = all_metronomes.len();
         let mut all_orderings = Vec::with_capacity(num_nodes);
@@ -136,7 +157,8 @@ mod tests {
         let num_ops = all_orderings[0].len();
         let mut h: HashMap<usize, usize> = HashMap::from_iter((0..num_ops).map(|x| (x, 0)));
         for column in 0..num_ops {
-            for ordering in &all_orderings { // row
+            for ordering in &all_orderings {
+                // row
                 let op_id = ordering[column];
                 h.insert(op_id, h[&op_id] + 1);
             }
@@ -171,12 +193,15 @@ mod tests {
 
         let ordered_tuples = Metronome::maximize_distance_ordering(&mut tuples);
 
-        assert_eq!(ordered_tuples, vec![
-            vec![1, 2, 3],
-            vec![10, 11, 12],
-            vec![4, 5, 6],
-            vec![7, 8, 9],
-        ]);
+        assert_eq!(
+            ordered_tuples,
+            vec![
+                vec![1, 2, 3],
+                vec![10, 11, 12],
+                vec![4, 5, 6],
+                vec![7, 8, 9],
+            ]
+        );
     }
 
     #[test]
@@ -188,7 +213,12 @@ mod tests {
             for pid in 1..=num_nodes as NodeId {
                 let m = Metronome::with(pid, num_nodes, quorum_size);
                 if pid == 1 {
-                    println!("N={}: ordering len: {}, critical len: {}", num_nodes, m.my_ordering.len(), m.critical_len);
+                    println!(
+                        "N={}: ordering len: {}, critical len: {}",
+                        num_nodes,
+                        m.my_ordering.len(),
+                        m.critical_len
+                    );
                 }
                 all_metronomes.push(m);
             }
